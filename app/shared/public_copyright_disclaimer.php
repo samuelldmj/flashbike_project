fc17dd2b34dbbf7b332af29e4b595ca0b9f0373c
Samuel <samuelldmj5@gmail.com>
2024-03-20 13:00:05 +0100

    flash bike oop

---
+++ app/shared/public_copyright_disclaimer.php
@@ -0,0 +1,3 @@
+<p>Copyright <?php echo date('Y'); ?>, Flash Bike</p>
+
+<p>This is a fictitious company created by <a href="https://twitter.com/samuelldmj">@Samuelldmj</a>, solely for the creation and development of educational training materials. Any resemblance to real products or services is purely coincidental. Information provided about the products or services is also fictitious and should not be construed as representative of actual products or services on the market in a similar product or service category.</p>
---
+++ public/about.php
@@ -0,0 +1,21 @@
+<?php require_once('../app/initialize.php'); ?>
+
+<?php $page_title = 'About Us'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page" class="narrow">
+    <h2>About Us</h2>
+    <p>Flash Bike was started in 2000 by a small group of bicycle loving buddies.
+       We wanted to create a neighborhood bike shop that could also deliver top-quality bicycles to your doorstep. 
+       Now with six locations around the city, we are able to reach even more neighborhoods.</p>
+    <p>Our mechanics inspect every bicycle from top to bottom before it leaves our shop.
+       If you have questions, our expert staff has the knowledge and experience to advise you and to meet all of your cycling needs.</p>
+  </div>
+  
+</div>
+
+<?php $super_hero_image = 'AdobeStock_50633008_xlarge.jpeg'; ?>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
